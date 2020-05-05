e1b609b89c7e33880d56e450481db88f43de6197
kolgha <64870104+kolgha@users.noreply.github.com>
2020-05-06 06:43:52 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,12 @@
-import discord
+from discord.ext import commands
+import os
+import traceback
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
+
+
+
 
 @client.event
 async def on_message(message):
