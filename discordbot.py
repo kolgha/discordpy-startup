9100b084af97ea535a2bfb6709d21e82e113056e
kolgha <64870104+kolgha@users.noreply.github.com>
2020-05-06 20:23:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='!')
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -16,7 +16,7 @@ async def on_ready():
     print('------')
 
 @client.command()
-async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
+async def rect(ctx, about = "募集", cnt = 4, settime = 1800.0):
     cnt, settime = int(cnt), float(settime)
     reaction_member = [">>>"]
     test = discord.Embed(title=about,colour=0x1e90ff)
@@ -24,7 +24,8 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
     msg = await ctx.send(embed=test)
     #投票の欄
     await msg.add_reaction('⏫')
-    await msg.add_reaction('✖')
+#    await msg.add_reaction('✖')
+
 
     def check(reaction, user):
         emoji = str(reaction.emoji)
@@ -52,7 +53,7 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
                     test = discord.Embed(title=about,colour=0x1e90ff)
                     test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
                     await msg.edit(embed=test)
-                    finish = discord.Embed(title=about,colour=0x1e90ff)
+                    finish = discord.Embed(title=about,colour=0x00FF00)
                     finish.add_field(name="おっと、メンバーがきまったようだ",value='\n'.join(reaction_member), inline=True)
                     await ctx.send(embed=finish)
 
@@ -63,10 +64,11 @@ async def rect(ctx, about = "募集", cnt = 4, settime = 10.0):
                     test = discord.Embed(title=about,colour=0x1e90ff)
                     test.add_field(name=f"あと__{cnt}__人 募集中\n", value='\n'.join(reaction_member), inline=True)
                     await msg.edit(embed=test)
+
                 else:
                     pass
         # リアクション消す。メッセージ管理権限がないとForbidden:エラーが出ます。
         await msg.remove_reaction(str(reaction.emoji), user)
-     
+
 
 client.run(token)
